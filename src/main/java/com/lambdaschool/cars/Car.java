54f7ffb814f1487866b11e01fec7558eaf4955e2
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-02-15 09:00:33 -0800

    Added new file, Car.java; setup fields, imports, default constructor & reg constructor

---
+++ src/main/java/com/lambdaschool/cars/Car.java
@@ -0,0 +1,32 @@
+package com.lambdaschool.cars;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+
+public class Car
+{
+    // fields
+    private @Id @GeneratedValue Long id;
+    private int year;
+    private String brand;
+    private String model;
+
+    public Car()
+    {
+        // default constructor
+    }
+
+    // constructor
+    public Car(int year, String brand, String model)
+    {
+        this.year = year;
+        this.brand = brand;
+        this.model = model;
+    }
+}
